e8ed24bf2788055a5127207c47e9142346f3a99f
alessiosavi <alessiosavibtc@gmail.com>
2021-04-19 10:23:04 +0200

    Add log to file

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"path"
 	"sort"
 	"strings"
+	"time"
 
 	fileutils "github.com/alessiosavi/GoGPUtils/files"
 )
@@ -27,6 +28,17 @@ const LTC_FILE_USD = `data/ltc-usd_%s.json`
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Llongfile)
+	timeNow := time.Now().Format("2006.01.02_15.04.05")
+	if !fileutils.IsDir("log") {
+		os.Mkdir("log", 0755)
+	}
+	f, err := os.OpenFile(fmt.Sprintf("log/log_%s.log", timeNow), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
 	b := flag.Bool("merge", false, "merge data")
 	flag.Parse()
 	if *b {
@@ -71,7 +83,7 @@ func MergeData(target, finalName string) {
 
 	fName := path.Join("data/", finalName)
 
-	// If the file doesn't exist, create it, or append to the file
+	// If the file doesn't exist, create it
 	f, err := os.OpenFile(fName, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
